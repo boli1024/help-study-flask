703d15b9f58f17dd4641bf5f854942762f869096
boli <729532969@qq.com>
2018-12-13 21:12:40 +0800

    flask-bootstrap框架换为bootstrap-flask 添加后台管理

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap, WebCDN, ConditionalCDN, BOOTSTRAP_VERSION, JQUERY_VERSION, HTML5SHIV_VERSION, RESPONDJS_VERSION
+# from flask_bootstrap import Bootstrap, WebCDN, ConditionalCDN, BOOTSTRAP_VERSION, JQUERY_VERSION, HTML5SHIV_VERSION, RESPONDJS_VERSION
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import config
@@ -24,6 +25,10 @@ def create_app(config_name):
     from .routes.video import main as route_video
     from .auth import auth as route_auth
     from .routes.test import test as route_test
+    from .routes.manage_student import main as route_manage_student
+    from .routes.manage_direction import main as route_manage_direction
+    app.register_blueprint(route_manage_student, url_prefix='/studentManage')
+    app.register_blueprint(route_manage_direction, url_prefix='/directionManage')
     app.register_blueprint(route_project, url_prefix='')
     app.register_blueprint(route_film, url_prefix='/film')
     app.register_blueprint(route_video, url_prefix='/video')
@@ -31,7 +36,7 @@ def create_app(config_name):
     app.register_blueprint(route_test, url_prefix='/test')
 
     bootstrap.init_app(app)
-    change_cdn_domestic(app)
+    # change_cdn_domestic(app)
     db.init_app(app)
     login_manager.init_app(app)
 
@@ -42,13 +47,13 @@ def change_cdn_domestic(tar_app):
     static = tar_app.extensions['bootstrap']['cdns']['static']
     local = tar_app.extensions['bootstrap']['cdns']['local']
 
-    def change_one(tar_lib, tar_ver, fallback):
-        tar_js = ConditionalCDN('BOOTSTRAP_SERVE_LOCAL', fallback, WebCDN('//cdn.bootcss.com/' + tar_lib + '/' + tar_ver + '/'))
-        tar_app.extensions['bootstrap']['cdns'][tar_lib] = tar_js
-
-    libs = {'jquery': {'ver': JQUERY_VERSION, 'fallback': local},
-            'bootstrap': {'ver': BOOTSTRAP_VERSION, 'fallback': local},
-            'html5shiv': {'ver': HTML5SHIV_VERSION, 'fallback': static},
-            'respond.js': {'ver': RESPONDJS_VERSION, 'fallback': static}}
-    for lib, par in libs.items():
-        change_one(lib, par['ver'], par['fallback'])
+    # def change_one(tar_lib, tar_ver, fallback):
+    #     tar_js = ConditionalCDN('BOOTSTRAP_SERVE_LOCAL', fallback, WebCDN('//cdn.bootcss.com/' + tar_lib + '/' + tar_ver + '/'))
+    #     tar_app.extensions['bootstrap']['cdns'][tar_lib] = tar_js
+    #
+    # libs = {'jquery': {'ver': JQUERY_VERSION, 'fallback': local},
+    #         'bootstrap': {'ver': BOOTSTRAP_VERSION, 'fallback': local},
+    #         'html5shiv': {'ver': HTML5SHIV_VERSION, 'fallback': static},
+    #         'respond.js': {'ver': RESPONDJS_VERSION, 'fallback': static}}
+    # for lib, par in libs.items():
+    #     change_one(lib, par['ver'], par['fallback'])
--- config.py
+++ config.py
@@ -15,6 +15,7 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URL") or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    PER_PAGE = 10
 
 
 config = {
